00386122810e53e1e7bb0b6b16ed04cdbe71ab36
Michael <micjanic20@gmail.com>
2020-11-03 23:10:18 -0600

    snake movement

--- src/App.js
+++ src/App.js
@@ -14,16 +14,27 @@ const App = () => {
     const [snake, setSnake] = useState(SNAKE_START)
     const [apple, setApple] = useState(APPLE_START)
     const [dir, setDir] = useState([0, -1])
-    const [speed, setSpeed] = useState(null)
+    const [speed, setSpeed] = useState(1000)
     const [gameOver, setGameOver] = useState(false)
 
     const startGame = () => {}
     const endGame = () => {}
-    const moveSnake = () => {}
+    const moveSnake = ({ keyCode }) => {
+        keyCode >= 37 && keyCode <= 40 && setDir(DIRECTIONS[keyCode])
+    }
     const createApple = () => {}
     const checkCollision = () => {}
     const checkAppleCollision = () => {}
-    const gameLoop = () => {}
+    const gameLoop = () => {
+        const snakeCopy = JSON.parse(JSON.stringify(snake)) //good way to deep clone
+        const newSnakeHead = [
+            snakeCopy[0][0] + dir[0],
+            snakeCopy[0][1] + dir[1],
+        ]
+        snakeCopy.unshift(newSnakeHead)
+        snakeCopy.pop()
+        setSnake(snakeCopy)
+    }
 
     useEffect(() => {
         const context = canvasRef.current.getContext('2d')
@@ -34,6 +45,8 @@ const App = () => {
         context.fillStyle = 'lightblue'
         context.fillRect(apple[0], apple[1], 1, 1)
     }, [snake, apple, gameOver])
+
+    useInterval(() => gameLoop(), speed)
 
     return (
         <div role="button" tabIndex="0" onKeyDown={(e) => moveSnake(e)}>
